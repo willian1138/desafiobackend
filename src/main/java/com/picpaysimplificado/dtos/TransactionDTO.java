6c606cc1045dab861e945cbe4ebb0a572eaa3d7f
William Silva <psilva546@hotmail.com>
2024-01-11 04:54:38 -0300

    transaction parcialmente concluido com a implementacao de DTO e o service

---
+++ src/main/java/com/picpaysimplificado/dtos/TransactionDTO.java
@@ -0,0 +1,9 @@
+package com.picpaysimplificado.dtos;
+
+import java.math.BigDecimal;
+
+public class TransactionDTO {
+    private BigDecimal amount;
+    private Long senderId;
+    private Long receiverId;
+}
